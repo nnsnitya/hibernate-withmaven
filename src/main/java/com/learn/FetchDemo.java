5eba9be7fcee954c07c4a084de571d0955eda77f
Nityanand Singh <nns.nitya@gmail.com>
2024-02-08 20:21:14 +0530

    fetching data

---
+++ src/main/java/com/learn/FetchDemo.java
@@ -0,0 +1,36 @@
+package com.learn;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.learn.entities.Address;
+import com.learn.entities.Student;
+
+public class FetchDemo {
+
+	public static void main(String[] args) {
+		
+		//get and load
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+
+        //get-student:101
+        Student student = session.get(Student.class, 103);
+        System.out.println(student.getCity());
+        
+        //load - student:102
+        Student student2 = session.load(Student.class, 102);
+        System.out.println(student2);
+        
+        Address ad = session.get(Address.class, 1);
+        System.out.println(ad);
+        
+        session.close();
+        factory.close();
+	}
+
+}
