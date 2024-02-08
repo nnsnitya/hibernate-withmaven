3fbdc977d0812c903d6cf4e2f2c189ac0c83db14
Nityanand Singh <nns.nitya@gmail.com>
2024-02-08 20:01:46 +0530

    saving image to db

--- src/main/java/com/learn/App.java
+++ src/main/java/com/learn/App.java
@@ -1,10 +1,16 @@
 package com.learn;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.learn.entities.Address;
 import com.learn.entities.Student;
 
 /**
@@ -13,7 +19,7 @@ import com.learn.entities.Student;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println("Project Started...");
         Configuration cfg = new Configuration();
@@ -22,18 +28,32 @@ public class App
         
         //creating student
         Student st = new Student();
-        st.setId(101);
-        st.setName("Sumit");
-        st.setCity("Delhi");
+        st.setId(103);
+        st.setName("Mukesh");
+        st.setCity("Patna");
         System.out.println(st);
+        
+        //creating obj of address class
+        Address ad = new Address();
+        ad.setStreet("street 1");
+        ad.setCity("Delhi");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(1234.23);
+        
+        //reading Image
+        FileInputStream fis = new FileInputStream("src/main/java/pic2.png");
+        byte[] data = new byte[fis.available()];
+        fis.read();
+        ad.setImage(data);
         
         Session session = factory.openSession();
         Transaction tx = session.beginTransaction();
         session.save(st);
+        session.save(ad);
         tx.commit();
         session.close();
         
-        System.out.println(factory);
-        System.out.println(factory.isClosed());
+        System.out.println("Done...");
     }
 }
