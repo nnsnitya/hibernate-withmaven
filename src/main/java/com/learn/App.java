68f3a78ec4bbc7b10fa6a7200e7bc44eaab43fc5
Nityanand Singh <nns.nitya@gmail.com>
2024-02-08 18:47:23 +0530

    all set
    openSession(), -> beginTransaction() -> save() -> getTransaction().commit
    session.close

--- src/main/java/com/learn/App.java
+++ src/main/java/com/learn/App.java
@@ -1,7 +1,10 @@
 package com.learn;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import com.learn.entities.Student;
 
 /**
  * Hello world!
@@ -11,11 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Project Started..." );
+        System.out.println("Project Started...");
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         
-        System.out.println(factory);
+        //creating student
+        Student st = new Student();
+        st.setId(102);
+        st.setName("Amit");
+        st.setCity("Chennai");
+        System.out.println(st);
+        
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.save(st);
+        session.getTransaction().commit();
+        session.close();
+        
+//        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
